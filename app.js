ba711defb6df08da4740186083998a0498103aed
Kfir Stri <kfirstar@gmail.com>
2017-03-22 19:43:43 +0200

    some order to files ad folder, added endpoints

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// controllers
+var commentsController = require('./controllers/comments');
+
+// handle static files
+app.use(express.static('public'));
+
+// create comment HTTP endpoints
+app.get('/comments', commentsController.getAllComments);
+app.post('/comment', commentsController.saveNewComment);
+
+// start server
+app.listen(1337, () => {
+  console.log('server started at http://localhost:1337');
+});
---
+++ controllers/comments.js
@@ -0,0 +1,9 @@
+var getAllComments = (req, res) => {
+  console.log('get all comments');
+}
+
+var saveNewComment = (req, res) => {
+  console.log('save new comment');
+}
+
+module.exports = { getAllComments, saveNewComment };
