7cb88397d3a17fdf73c4b839e27e816046b45549
Kfir Stri <kfirstar@gmail.com>
2017-03-24 09:37:41 +0300

    client

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,9 +1,11 @@
 var getAllComments = (req, res) => {
   console.log('get all comments');
+  return res.send("Hello all comments");
 }
 
 var saveNewComment = (req, res) => {
   console.log('save new comment');
+  return res.send("saved all comments");
 }
 
 module.exports = { getAllComments, saveNewComment };
